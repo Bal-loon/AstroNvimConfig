3dff0476714d5a5af9d0a20a76a37ea4a618ec64
Joe Tang <joe.tang@activenetwork.com>
2024-04-17 13:40:46 +0800

    chore: Add noice settings

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,29 @@
+return {
+  "folke/noice.nvim",
+  opts = function(_, config)
+    config.views = {
+      cmdline_popup = {
+        position = {
+          row = 20,
+          col = "50%",
+        },
+        border = {
+          style = "none",
+          padding = { 1, 1 },
+        },
+        filter_options = {},
+        win_options = {
+          winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+        },
+      },
+      popupmenu = {
+        -- relative = "editor",
+        position = {
+          row = 28,
+          col = "50%",
+        },
+      },
+    }
+    return config
+  end,
+}
