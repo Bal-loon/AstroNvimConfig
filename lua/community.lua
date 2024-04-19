a7773eeb8b1b483be976031cc8b876d6d3352846
Joe Tang <joe.tang@activenetwork.com>
2024-04-19 13:26:54 +0800

    chore: Add community plugin nvchad statusline

--- lua/community.lua
+++ lua/community.lua
@@ -10,5 +10,6 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.utility.noice-nvim" },
+  { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
   -- import/override with your plugins folder
 }
